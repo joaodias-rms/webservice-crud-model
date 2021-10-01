308f3d88df3acfc90d06d557e17ea49f4bee4e82
joao <joao.victor@topsapp.com.br>
2021-10-01 14:38:16 -0400

    created crud routes

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
 
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
-  res.json({ mensagem: "rota funcionando" });
+  res.json({ mensagem: "Consulta de todos os registro" });
 });
 
-app.listen(3000, ()=>{
-    console.log('Tudo ok');
-})
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensagem: `consulta somente do id: ${id}` });
+});
+
+app.post("/", (req, res) => {
+  const body = req.body;
+  res.json(body);
+});
+
+app.put("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensagem: `atualizar somente o id: ${id}` });
+});
+
+app.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensage: `deletar somente o id: ${id}` });
+});
+
+app.listen(3000, () => {
+  console.log("Tudo ok");
+});
