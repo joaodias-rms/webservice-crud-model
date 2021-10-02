a27d0744aa8b2b26f4e2765f12cfed7e9352f464
João <jvd.ramos@hotmail.com>
2021-10-02 18:54:38 -0300

    refactor on routes

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const app = express();
 
+const routes = require("./src/routes/main.routes");
+mongoose.connect("mongodb://localhost:27017/ws-netflix", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+});
+
+app.use(bodyParser.json());
 app.use(morgan("dev"));
-
-app.get("/", (req, res) => {
-  res.json({ mensagem: "Consulta de todos os registro" });
-});
-
-app.get("/:id", (req, res) => {
-  const id = req.params.id;
-  res.json({ mensagem: `consulta somente do id: ${id}` });
-});
-
-app.post("/", (req, res) => {
-  const body = req.body;
-  res.json(body);
-});
-
-app.put("/:id", (req, res) => {
-  const id = req.params.id;
-  res.json({ mensagem: `atualizar somente o id: ${id}` });
-});
-
-app.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  res.json({ mensage: `deletar somente o id: ${id}` });
-});
+app.use("/", routes);
 
 app.listen(3000, () => {
   console.log("Tudo ok");
